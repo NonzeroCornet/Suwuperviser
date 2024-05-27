6f716610730a2b96e9276d380d6b5fd898ad93b6
NonzeroCornet34 <ullerbri000@gmail.com>
2024-05-26 21:34:31 -0600

    Update port handling

--- index.js
+++ index.js
@@ -58,5 +58,5 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Running on port ${PORT}`);
 });
